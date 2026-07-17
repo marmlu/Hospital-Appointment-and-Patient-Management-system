2848452ed83f0031321c22ec76b529782e37b25f
medanit <medibrhanu86@gmail.com>
2026-07-16 23:30:52 -0700

    Add medical record and prescription model relationships

--- app/Models/Medicine.php
+++ app/Models/Medicine.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medicine extends Model
 {
-    //
+    protected $fillable = [
+        'medicine_name',
+        'description',
+        'dosage_form',
+        'manufacturer',
+    ];
+
+    public function prescriptionItems()
+    {
+        return $this->hasMany(PrescriptionItem::class);
+    }
 }

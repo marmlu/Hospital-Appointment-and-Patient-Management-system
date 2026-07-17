f254f656632204ae29dc0c05c833fd3935de1e6f
marmlu <dagnenuhamin987@gmail.com>
2026-07-17 15:26:55 +0300

    Creat model fo appointment table and write the model code

---
+++ app/Models/Appointment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    protected $fillable = [
+        'patient_id',
+        'doctor_id',
+        'appointment_date',
+        'appointment_time',
+        'reason',
+        'status',
+        'notes',
+        'appointment_type',
+    ];
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+}

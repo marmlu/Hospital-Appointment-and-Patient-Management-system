3e99f1818c68297608e9be138dae31cada0e965b
marmlu <dagnenuhamin987@gmail.com>
2026-08-11 19:19:26 +0300

    Add status filter logic and combine it with appointment search

--- appointment-frontend/js/script.js
+++ appointment-frontend/js/script.js
@@ -1,14 +1,24 @@
 const appointmentSearch = document.querySelector(".appointment-search-input");
 const appointmentRows = document.querySelectorAll("tbody tr");
+const statusFilter = document.querySelector(".status-filter");
 
-appointmentSearch.addEventListener("input", function () {
+function filterAppointments() {
     const searchValue = appointmentSearch.value.toLowerCase();
+    const selectedStatus = statusFilter.value.toLowerCase();
     appointmentRows.forEach(function (row) {
         const rowText = row.textContent.toLowerCase();
-        if (rowText.includes(searchValue)) {
+        const statusCell = row.querySelector("td:nth-child(8)");
+        const statusText = statusCell.textContent.toLowerCase();
+        if (
+            rowText.includes(searchValue) &&
+            (statusText === selectedStatus || selectedStatus === "all status")
+        ) {
             row.style.display = "";
         } else {
             row.style.display = "none";
         }
     });
-});
+}
+
+appointmentSearch.addEventListener("input", filterAppointments);
+statusFilter.addEventListener("change", filterAppointments);

3e1cb3104d2577e5de363f9ff7d61fe268746b72
marmlu <dagnenuhamin987@gmail.com>
2026-08-11 20:01:49 +0300

    Combine department filter logic with appointment search and status filter logic

--- appointment-frontend/js/script.js
+++ appointment-frontend/js/script.js
@@ -1,17 +1,23 @@
 const appointmentSearch = document.querySelector(".appointment-search-input");
 const appointmentRows = document.querySelectorAll("tbody tr");
 const statusFilter = document.querySelector(".status-filter");
-
+const departmentFilter = document.querySelector(".department-filter");
 function filterAppointments() {
     const searchValue = appointmentSearch.value.toLowerCase();
     const selectedStatus = statusFilter.value.toLowerCase();
+    const selectedDepartment = departmentFilter.value.toLowerCase();
     appointmentRows.forEach(function (row) {
         const rowText = row.textContent.toLowerCase();
         const statusCell = row.querySelector("td:nth-child(8)");
+        const departmentCell = row.querySelector("td:nth-child(4)");
         const statusText = statusCell.textContent.toLowerCase();
+        const departmentText = departmentCell.textContent.toLowerCase();
         if (
             rowText.includes(searchValue) &&
-            (statusText === selectedStatus || selectedStatus === "all status")
+            (statusText === selectedStatus ||
+                selectedStatus === "all status") &&
+            (departmentText === selectedDepartment ||
+                selectedDepartment === "all departments")
         ) {
             row.style.display = "";
         } else {
@@ -22,3 +28,4 @@ function filterAppointments() {
 
 appointmentSearch.addEventListener("input", filterAppointments);
 statusFilter.addEventListener("change", filterAppointments);
+departmentFilter.addEventListener("change", filterAppointments);

b47cd04381423a2b6b7873f58ff92d5124e40176
Mekdes Erdachew <mekdeserdachew@gmail.com>
2026-08-28 07:20:41 -0700

    Keep only frontend and backend projects

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DepartmentController;
-use App\Http\Controllers\DoctorController;
-
-Route::apiResource('departments', DepartmentController::class);
-Route::apiResource('doctors', DoctorController::class);
